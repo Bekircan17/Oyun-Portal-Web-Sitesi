da3cfd64338485f631a7c206f000e4fbb1d97cbb
Bekircan17 <bekircankaymak18@gmail.com>
2023-03-26 18:57:57 +0300

    Oyun Portal Web Sitesi

---
+++ js/main.js
@@ -0,0 +1,27 @@
+//Custom Owl Corousel
+
+$(document).ready(function(){
+    let owl= $(".owl-carousel").owlCarousel({
+      autoplay :false,
+      dots :true,
+      dotsData:true,
+      loop:true,
+      nav:false,
+      items :1
+
+    });
+
+      $('.owl-dot').click(function(){
+          owl.trigger('to.owl.carousel', [$(this).index(), 1000]);
+      })
+  });
+
+  //Open -- close menu
+
+  $('#chek').change(function(){
+    if($(this).is(":checked")){
+      $('.nav-header').addClass('active');
+    } else{
+      $('.nav-header').removeClass('active');
+    }
+  });
